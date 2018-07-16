5cb7a876b34bcead6df3c9775ab337149bbfa09d
Ceri Westcott <ceribusiness@gmail.com>
2018-07-16 13:32:58 +0100

    Add project files.

---
+++ Timesheets - Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Timesheets___Assignment.Startup))]
+namespace Timesheets___Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
